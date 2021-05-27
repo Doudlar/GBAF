8761cb8e26b25c6678707baefe35638862c3229a
Edouard Fournier <edouardj.fournier@gmail.com>
2021-05-27 12:29:06 +0200

    Mise en place du HTML et CSS des pages accueil et acteur

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<header>
+			<a href='accueil.php'><img src='ressources/LOGO_GBAF_ROUGE.png' titre='logo GBAF' alt="Logo de la GBAF" id="logo"/></a>
+			<div id='user'><a href='compte.php'><img src='ressources/user.png' titre ='avatar' alt='avatar'/></a><a href='compte.php'><p>
+				<?php echo'Edouard Fournier' ?></p></a></div>
+		</header>
+	</body>
+</html>
